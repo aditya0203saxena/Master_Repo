177f770932c3e1a590d078f016f5291d5ca7ffa2
Aditya Anand Saxena <aditya.saxena.research@gmail.com>
2026-09-13 14:55:52 +0530

    fix: align window timer return type for CareGuard

--- types/browser-timers.d.ts
+++ types/browser-timers.d.ts
@@ -1,1 +1,9 @@
 declare function setTimeout(handler: (...args: unknown[]) => void, timeout?: number, ...args: unknown[]): number;
+
+declare global {
+  interface Window {
+    setTimeout(handler: (...args: unknown[]) => void, timeout?: number, ...args: unknown[]): NodeJS.Timeout;
+  }
+}
+
+export {};
